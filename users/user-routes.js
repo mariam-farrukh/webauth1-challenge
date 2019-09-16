574edc7d93e7facc142373eb27aa06f934c32958
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-16 16:41:56 -0500

    Fixed post error on login

--- users/user-routes.js
+++ users/user-routes.js
@@ -46,7 +46,7 @@ router.post("/login", (req, res) => {
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
-          req.session.username = user.username;
+        //   req.session.username = user.username;
           res.status(200).json({ message: `${user.username} is logged in` });
         } else {
           res.status(401).json({ message: "Invalid Credentials" });
