19d343de7b195840c3f857311f24b275d743e8ab
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-16 16:18:26 -0500

    Created routers, working except login

---
+++ auth/restricted.js
@@ -0,0 +1,24 @@
+const bcrypt = require("bcryptjs");
+
+const Users = require("../users/user-model.js");
+
+module.exports = function restricted(req, res, next) {
+    const { username, password } = req.headers;
+  
+    if (username && password) {
+      Users.findUser({ username })
+        .first()
+        .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+            next();
+          } else {
+            res.status(401).json({ message: "Invalid Credentials" });
+          }
+        })
+        .catch(err => {
+          res.status(500).json({ message: "Server Error" });
+        });
+    } else {
+      res.status(400).json({ message: "No credentials Provided" });
+    }
+}
